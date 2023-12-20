07033412af449980241f926545803bfdd68ec246
Farid Lotfi <138003177+fariidlotfi@users.noreply.github.com>
2023-12-20 11:48:27 +0330

    Change getting data method from Fetch to Axios. also create a custom Modal to show error

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,10 @@ import {
 } from "react-icons/io5";
 import { FaWind } from "react-icons/fa6";
 
+import axios from "axios";
+
+import Modal from "./Components/Modal.jsx";
+
 function App() {
   const [reqCity, setReqCity] = useState();
 
@@ -23,76 +27,99 @@ function App() {
   const [windDeg, setWindDeg] = useState();
   const [uvIndex, setUVIndex] = useState();
 
+  const [cityError, setCityError] = useState(false);
+
   async function getData(request) {
-    const response = await fetch(
-      `https://api.weatherapi.com/v1/current.json?key=ee11d507ce7743c5a7564546231312&q=${request}`
-    );
-    const res = await response.json();
-    //console.log(res);
-    setCity(res.location.name + ", " + res.location.region);
-    setCondition(res.current.condition.text);
-    setConditionIcon(res.current.condition.icon);
+    const res = await axios
+      .get(
+        `https://api.weatherapi.com/v1/current.json?key=ee11d507ce7743c5a7564546231312&q=${request}`,
+      )
+      .then((response) => response.data)
+      .catch(() => {
+        setCityError(true);
+      });
 
-    const tempDate = res.location.localtime;
-    setDate(tempDate.split(" "));
+    if (cityError) {
+      return;
+    } else {
+      setCity(res.location.name + ", " + res.location.region);
+      setCondition(res.current.condition.text);
+      setConditionIcon(res.current.condition.icon);
 
-    setTemp(res.current.temp_c);
-    setHumidity(res.current.humidity);
-    setWindK(res.current.wind_kph);
-    setWindDeg(res.current.wind_degree);
-    setUVIndex(res.current.uv);
+      const tempDate = res.location.localtime;
+      setDate(tempDate.split(" "));
+
+      setTemp(res.current.temp_c);
+      setHumidity(res.current.humidity);
+      setWindK(res.current.wind_kph);
+      setWindDeg(res.current.wind_degree);
+      setUVIndex(res.current.uv);
+    }
   }
 
   useEffect(() => {
     getData("marivan");
   }, []);
 
+  const closeModal = () => {
+    setCityError(false);
+  };
+
   return (
-    <div className="container">
-      <div className="inputs box-style">
-        <input
-          type="text"
-          value={reqCity}
-          onChange={(e) => setReqCity(e.target.value)}
-          onKeyDown={(e) => (e.key == "Enter" ? getData(reqCity) : null)}
+    <>
+      {cityError && (
+        <Modal
+          key={Math.floor(Math.random() * 1000)}
+          close={closeModal}
+          title={"Enter City Name"}
+          desc={"Please enter the city name correctly!"}
         />
-        <button onClick={() => getData(reqCity)}>check</button>
-      </div>
-      <div className="city-info box-style">
-        <div className="city-info-header">
-          <span>
-            <MdOutlineMyLocation />
-            {city && city}
-          </span>
-          <p>{date && date[1]}</p>
+      )}
+      <div className="container">
+        <div className="inputs box-style">
+          <input
+            type="text"
+            value={reqCity}
+            onChange={(e) => setReqCity(e.target.value)}
+            onKeyDown={(e) => (e.key == "Enter" ? getData(reqCity) : null)}
+          />
+          <button onClick={() => getData(reqCity)}>check</button>
         </div>
-        <div className="city-info-data">
-          <img src={conditionIcon} height="150%" />
-          <div>
-            <p className="temp">{temp && temp}</p>
-            <p>{condition && condition}</p>
+        <div className="city-info box-style">
+          <div className="city-info-header">
+            <span>
+              <MdOutlineMyLocation />
+              {city && city}
+            </span>
+            <p>{date && date[1]}</p>
+          </div>
+          <div className="city-info-data">
+            <img src={conditionIcon} height="150%" />
+            <div>
+              <p className="temp">{temp && temp}</p>
+              <p>{condition && condition}</p>
+            </div>
+          </div>
+          <div className="city-info-date">
+            <IoCalendarOutline /> <p>{date && date[0]}</p>
           </div>
         </div>
-        <div className="city-info-date">
-          <IoCalendarOutline /> <p>{date && date[0]}</p>
+        <div className="other-city-info box-style">
+          <Icon title="Humidity" number={humidity}>
+            <WiHumidity color="#7a34e2" />
+          </Icon>
+          <Icon title="Wind Speed" number={windK}>
+            <FaWind color="#7a34e2" />
+          </Icon>
+          <Icon title="Wind Degree" number={windDeg}>
+            <IoRainyOutline color="#7a34e2" />
+          </Icon>
+          <Icon title="UV Index" number={uvIndex}>
+            <IoSunnyOutline color="#7a34e2" />
+          </Icon>
         </div>
       </div>
-
-      <div className="other-city-info box-style">
-        <Icon title="Humidity" number={humidity}>
-          <WiHumidity color="#7a34e2" />
-        </Icon>
-        <Icon title="Wind Speed" number={windK}>
-          <FaWind color="#7a34e2" />
-        </Icon>
-        <Icon title="Wind Degree" number={windDeg}>
-          <IoRainyOutline color="#7a34e2" />
-        </Icon>
-        <Icon title="UV Index" number={uvIndex}>
-          <IoSunnyOutline color="#7a34e2" />
-        </Icon>
-      </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/Components/Modal.jsx
@@ -0,0 +1,22 @@
+import "./Modal.css";
+const Modal = (params) => {
+  const { title, desc, close } = params;
+
+  return (
+    <div className="modal-container">
+      <div className="modal-body" onClick={() => close()}>
+        <img
+          src="src/assets/close.svg"
+          width="30px"
+          height="30px"
+          alt={"error-icon"}
+        />
+        <h2>{title}</h2>
+        <p>{desc}</p>
+        <button className="modal-close">Close</button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
